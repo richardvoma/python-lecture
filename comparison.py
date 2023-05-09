8494e46af56be8c1ee5f3469c002ab793fe35ec6
richardvoma <richardvoma1@gmail.com>
2023-05-08 23:11:23 -0400

    quiz

---
+++ comparison.py
@@ -0,0 +1,15 @@
+# Write a program that asks the user to enter two numbers and compares them using the following comparison operators: `>`, `<`, `>=`, `<=`, `==`, and `!=`. 
+# The program should print out the result of each comparison.
+# Write a program that asks the user to enter two numbers and compares them using the following comparison
+voma = 5
+relindis = 2
+
+voma = int(input("type a number:"))
+relindis = int(input("type another number:"))
+
+print(voma > relindis)  # voma greater than relindis
+print(relindis < voma)  # less than
+print(voma >= relindis)  # greater than or equal
+print(relindis <= voma)  # less than or equal to
+print(relindis == voma)  # equall to
+print(voma != relindis)  # not equal to
---
+++ operators.py
@@ -0,0 +1,48 @@
+W = 10
+S = 3
+print(W)
+
+S += 13  # S=S+13
+print(S)
+q = 10
+q -= 20  # q=q-20
+print(q)
+p = 32
+p %= 7
+print(p)
+u = 20
+u //= 3
+print(u)
+z = 10
+z /= 3
+print(z)
+# python comparison operators
+x = 10
+y = 4
+
+# equality operator
+print(x == y)
+
+# greater than operator
+print(x > y)
+# greater than or equal to operator
+print(x >= y)
+
+# less than operator
+print(y < x)
+# less than or equal to
+print(x <= y)
+
+# logical operators
+foncho = 45
+richie = 30
+clovis = 10
+miriam = 20
+eliz = 5
+# the and operator
+print(richie > foncho and miriam > eliz)
+
+# the or operators
+print(clovis < miriam or eliz > foncho)
+# the not operator
+print(not (clovis < miriam or eliz > foncho))
